7ec6f09985c54ba865e89d7169aa261415cd6279
Matt Williams <matt.williams2@dc.gov>
2015-06-05 11:33:04 -0400

    Changed default db path to RedHat default

--- openhbx_mongodb/resources/install.rb
+++ openhbx_mongodb/resources/install.rb
@@ -4,7 +4,7 @@ attribute :name, :kind_of => String, :name_attribute => true, :required => true
 attribute :config_path, :kind_of => String, :default => "/etc"
 attribute :log_path, :kind_of => String, :default => "/var/log/mongodb"
 attribute :port, :kind_of => Integer, :default => 27017
-attribute :db_path, :kind_of => String, :default => "/var/lib/mongodb"
+attribute :db_path, :kind_of => String, :default => "/var/lib/mongo"
 attribute :version, :kind_of => String, :default => "2.6"
 attribute :pid_file_path, :kind_of => String, :default => "/var/run/mongodb"
 attribute :repo_path, :kind_of => String, :default => "/etc/yum.repos.d"
