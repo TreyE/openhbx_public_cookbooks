a70ae92a58602cb016ae80a5581467dff93a913f
Matt Williams <matt.williams2@dc.gov>
2015-06-09 17:17:26 -0400

    Fix remi repo rpm download/install

--- openhbx_redis/providers/install.rb
+++ openhbx_redis/providers/install.rb
@@ -6,18 +6,21 @@ action :install do
   version = new_resource.version
   redis_pkg = "redis-" + version
 
-  remote_file "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm" do
+  remote_file "/tmp/remi-release-6.rpm" do
     source "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
-    action :create
+    #not_if {::File.exists?("/opt/td-agent/usr/bin/td")}
+    notifies :install, "rpm_package[remi]", :immediately
+    retries 2 
   end
 
   package "jemalloc" do
     action :install
   end
 
-  rpm_package "remi-release" do
-    source "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm"
-    action :install
+  rpm_package "remi" do
+    source "/tmp/remi-release-6.rpm"
+    only_if {::File.exists?("/tmp/remi-release-6.rpm")}
+    action :nothing  
   end  
 
   package redis_pkg do
