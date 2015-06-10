3c05a67b80e0720a29b20ff307cd4d2790723e87
Matt Williams <matt.williams2@dc.gov>
2015-06-10 16:30:41 -0400

    Fixed redis 2.8 download and install

--- openhbx_redis/providers/install.rb
+++ openhbx_redis/providers/install.rb
@@ -6,21 +6,19 @@ action :install do
   version = new_resource.version
   redis_pkg = "redis-" + version
 
-  remote_file "/tmp/redis-2.8.rpm" do
-    source "http://dhsdcassvnsrv01.dhs.dc.gov:9395/drivers/redis-2.8.19-1.el6.remi.x86_64.rpm"
-    #not_if {::File.exists?("/opt/td-agent/usr/bin/td")}
-    notifies :install, "rpm_package[redis-2.8]", :immediately
-    retries 2 
-  end
-
   package "jemalloc" do
     action :install
   end
 
-  yum_package "redis-2.8" do
-    source "/tmp/redis-2.8.rpm"
-    only_if {::File.exists?("/tmp/redis-2.8.rpm")}
-    action :nothing  
+  execute 'get-redis' do
+    command '/usr/bin/curl --insecure -O https://dhsdcassvnsrv01.dhs.dc.gov:9395/drivers/redis-2.8.19-1.el6.remi.x86_64.rpm -o /tmp/redis-2.8.19-1.el6.remi.x86_64.rpm'
+    action :nothing
+  end
+
+  package "redis" do
+    source "/tmp/redis-2.8.19-1.el6.remi.x86_64.rpm"
+    action :install
+    provider Chef::Provider::Package::Rpm
   end  
 
   service "redis" do
