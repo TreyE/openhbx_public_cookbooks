4d200088154239b5c47b2004c2483a97aeeee50f
Trey Evans <lewis.r.evans@gmail.com>
2015-06-11 14:15:53 -0400

    Fixing installation.

--- openhbx_erlang/recipes/default.rb
+++ openhbx_erlang/recipes/default.rb
@@ -7,15 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
+["wxGTK", "wxGTK-devel", "unixODBC", "unixODBC-devel"].each do |pkg_name|
+  package pkg_name do
+    action :install
+  end
+end
+
 remote_file "/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm" do
   source "http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_3_general/esl-erlang_17.5-1~centos~6_amd64.rpm"
   not_if 'which erl'
-  notifies :install, "rpm_package[install_erlang]", :immediately
   retries 2 # We may be redirected to a FTP
 end
 
 rpm_package "install_erlang" do
-  action :nothing
+  action :install
   source "/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm"
-  only_if {::File.exists?("/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm")}
+  not_if 'which erl'
 end
