02eb5ed238e946d84a5703c07c8a61199ec4d8ef
Trey Evans <lewis.r.evans@gmail.com>
2015-06-11 12:07:44 -0400

    Adding simple erlang installation.

---
+++ openhbx_erlang/CHANGELOG.md
@@ -0,0 +1,13 @@
+openhbx_erlang CHANGELOG
+========================
+
+This file is used to list changes made in each version of the openhbx_erlang cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of openhbx_erlang
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ openhbx_erlang/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: openhbx_erlang
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file "/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm" do
+  source "http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_3_general/esl-erlang_17.5-1~centos~6_amd64.rpm"
+  not_if { `which -s erl`.blank? }
+  notifies :install, "rpm_package[install_erlang]", :immediately
+  retries 2 # We may be redirected to a FTP
+end
+
+rpm_package "erlang" do
+  action :nothing
+  source "/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm"
+  only_if {::File.exists?("/tmp/esl-erlang_17.5-1-centos-6_amd64.rpm")}
+end
